a250107079fd070feab3a108896244c3fd0bc8e7
Melissa Mae <melissamaedsouza20@gmail.com>
2024-09-13 13:38:46 +0530

    Update ProjectsSection.jsx

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -18,7 +18,7 @@ const projectsData = [
     description: "A cinema booking app design featuring a user-friendly interface allows users to reserve seats for movies, view showtimes, and obtain digital tickets seamlessly.",
     image: "/images/cinema.jpeg",
     tag: ["Projects"],
-    figmaUrl: "https://www.figma.com/community/file/1413802606243073240/bookmovies" // Add Figma URL here
+    figmaUrl: "https://www.figma.com/community/file/1413802606243073240/bookmovies"
   },
   {
     id: 3,
