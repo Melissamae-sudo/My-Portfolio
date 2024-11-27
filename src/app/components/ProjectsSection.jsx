82f4023f4c901fa2ef63eeb42b406bdb5dc47f7c
Melissa Mae <melissamaedsouza20@gmail.com>
2024-11-27 22:32:05 +0530

    Update ProjectsSection.jsx

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -7,13 +7,6 @@ import { motion, useInView } from "framer-motion";
 const projectsData = [
   {
     id: 1,
-    title: "ATM Simulator",
-    description: "A Java application that credits and debits money on a dummy Bank Account",
-    image: "/images/atm.png",
-    tag: ["Projects"],
-  },
-  {
-    id: 2,
     title: "A Cinema Booking App Design",
     description: "A cinema booking app design featuring a user-friendly interface allows users to reserve seats for movies, view showtimes, and obtain digital tickets seamlessly using Figma",
     image: "/images/1727548237623.jpg",
@@ -21,7 +14,7 @@ const projectsData = [
     figmaUrl: "https://www.figma.com/community/file/1413802606243073240/bookmovies"
   },
   {
-    id: 3,
+    id: 2,
     title: "Pick Me",
     description: "Helps users create, organize, and manage tickets for issues efficiently, making it easy to track and resolve problems",
     image: "/images/ticket.png",
@@ -29,7 +22,7 @@ const projectsData = [
     gitUrl: "https://github.com/Melissamae-sudo/Pick-Me-App",
   },
   {
-    id: 4,
+    id: 3,
     title: "Do Me List",
     description: " Track and complete to-do items with a clear overview of priorities and progress",
     image: "/images/dome.png",
@@ -37,7 +30,7 @@ const projectsData = [
     gitUrl: "https://www.figma.com/proto/pUmVliyIWpckLc36Z4iDFX/Mae-Music?page-id=0%3A1&node-id=64-45&node-type=canvas&viewport=472%2C110%2C0.06&t=bwVBtfl57hCrl2ZZ-1&scaling=scale-down&content-scaling=fixed",
   },
     {
-    id: 5,
+    id: 4,
     title: "Mae Music",
     description: "A fictional music streaming company providing an ad-free platform with millions of songs. It converts visitors into signups for the ad-free platform",
     image: "/images/Screenshot 2024-10-06 124655.png",
@@ -45,7 +38,7 @@ const projectsData = [
     gitUrl: "https://github.com/Melissamae-sudo/Do-Me-List",
   },
   {
-    id: 6,
+    id: 5,
     title: "ClosetPop- An Outfit generator",
     description: "A dynamic outfit generator built in Figma, featuring a model and customizable outfit options. Designed to help users visualize and experiment with styles, making it easy to find the perfect fit",
     image: "/images/Screenshot 2024-11-24 222842.png",
@@ -53,7 +46,7 @@ const projectsData = [
     gitUrl: "https://www.figma.com/design/XXKzosTCv7ppZrzMdwFTGy/outfit-generator?node-id=0-1&t=2MBb5RRm1oSa6dSU-1",
   },
    {
-    id: 7,
+    id: 6,
     title: "Advent Calendar",
     description: "Create a magical digital advent calendar and turn the holidays into 24 days of surprises! Perfect for long-distance friends, family, or your significant other, this personalized calendar is alternative to anything store-bought. Fill each day with unique surprises, thoughtful messages, or creative treats that will bring joy and connection throughout the season.",
     image: "/images/Screenshot 2024-11-27 214733.png",
